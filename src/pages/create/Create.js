dcf00de82a2820ea95d611fbd8828dfbfa1f9d52
sim <simrahnajeeb@gmail.com>
2023-01-30 17:13:57 +0500

    Added a recipe form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,61 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css' 
 
 export default function Create() {
+
+    const [title, setTitle] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault() //prevent reloading when form in submitted
+
+        console.log(title,method,cookingTime)
+
+    }
+
     return (
-        <div>
-            Create
+        <div className="create">
+            <h2 className="page-title">Add a New Recipe</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    <span>Recipe title:</span>
+                    <input 
+                        type="text"
+                        onChange={(e)=> setTitle(e.target.value)}
+                        value={title}
+                        required
+                    />    
+                </label>
+
+                {/* Ingredients go here */}
+
+                <label>
+                    <span>Recipe method:</span>
+                    <textarea 
+                    onChange={( e ) => setMethod(e.target.value)}
+                    value={method}
+                    required 
+                    />
+                </label>
+
+                <label>
+                    <span>Cooking time (minutes):</span>
+                    <input
+                        type="number"
+                        onChange={( e ) => setCookingTime(e.target.value)}
+                        value={cookingTime}
+                        required
+                    />
+                </label>
+
+                <button className="btn">submit</button>
+
+            </form>
         </div>
     )
 }
